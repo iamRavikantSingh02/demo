3c432af6ac0790aea58e9834afae3e7c7f209c77
Ravi_Gupta09 <ravi_gupta09@infosys.com>
2021-03-02 23:16:15 +0530

    Commit 2nd March 2021

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -10,10 +10,11 @@ import java.util.List;
 @RequestMapping(path="api/v1/student")
 public class StudentController {
 
-    private final StudentService studentService;
+    private final StudentService studentService ;
 
     @Autowired
     public StudentController(StudentService studentService) {
+
         this.studentService = studentService;
     }
 
